4cafda75f34bc87b7cb66e9c8e7e550dff169e5e
A.E.K Laine <laine.antti.e@gmail.com>
2016-02-05 20:14:55 +0200

    Fixed audio issues and snooze is working. Use HOUR_OF_DAY instead of HOUR for time operations (12 hour clocks suck).

--- app/src/main/java/info/iamlaine/h4/alarmclock/AlarmReceiver.java
+++ app/src/main/java/info/iamlaine/h4/alarmclock/AlarmReceiver.java
@@ -15,7 +15,7 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         System.out.println("Timer done");
-        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "WAKE UP!", Toast.LENGTH_SHORT).show();
         MainActivity.getInstace().beAnnoying();
         MainActivity.getInstace().updateTheTextView("Hello", R.id.textView);
     }
--- app/src/main/java/info/iamlaine/h4/alarmclock/MainActivity.java
+++ app/src/main/java/info/iamlaine/h4/alarmclock/MainActivity.java
@@ -32,15 +32,7 @@ public class MainActivity extends FragmentActivity {
         AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.alarm_2);
-        mediaPlayer.setLooping(true);
-
         setContentView(R.layout.activity_main);
-
-        /* Retrieve a PendingIntent that will perform a broadcast */
-        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
-
     }
 
     public static MainActivity getInstace() {
@@ -62,21 +54,28 @@ public class MainActivity extends FragmentActivity {
     }
 
     public void beAnnoying () {
-        System.out.println("Playin audio");
+        System.out.println("Playin' audio");
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm_2);
+        mediaPlayer.setLooping(true);
         mediaPlayer.start();
     }
 
     public void startAlarm(int hours, int minutes) {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
+        /* Retrieve a PendingIntent that will perform a broadcast */
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
         Calendar calendar = Calendar.getInstance();
-        //calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.set(Calendar.HOUR_OF_DAY, hours);
         calendar.set(Calendar.MINUTE, minutes);
 
         System.out.println("Setting Alarm for: " + hours + ":" + minutes);
 
         manager.setAlarmClock(new AlarmManager.AlarmClockInfo(calendar.getTimeInMillis(), pendingIntent), pendingIntent);
+
         Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
     }
 
@@ -84,7 +83,22 @@ public class MainActivity extends FragmentActivity {
         System.out.println("Stop Alarm");
         TextView textV1 = (TextView) findViewById(R.id.textView);
         textV1.setText(":)");
-        mediaPlayer.pause();
+
+        Intent intentstop = new Intent(this, AlarmReceiver.class);
+        PendingIntent senderstop = PendingIntent.getBroadcast(MainActivity.this,
+                0, intentstop, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarmManagerstop = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        alarmManagerstop.cancel(senderstop);
+
+        // Stop the playback, reset and set to null
+        // This is a silly but bombproof way of stopping audio
+        if (mediaPlayer != null ) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
     public void snooze(View v) {
@@ -94,9 +108,12 @@ public class MainActivity extends FragmentActivity {
         System.out.println("Snoozing for 2 minutes");
 
         Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(System.currentTimeMillis());
 
-        startAlarm(c.get(Calendar.HOUR), c.get(Calendar.MINUTE) + 2);
+        // Get the current time and add 2 minutes in milliseconds to it
+        c.setTimeInMillis(System.currentTimeMillis() + 2 * 60 * 1000);
+
+        // Calendar.HOUR = bad, I hate 12 hour clocks.
+        startAlarm(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE));
 
     }
 
